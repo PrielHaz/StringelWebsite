c4f6e153e0dbd2840e4c29046c8fbfc0f85c2bd6
PrielHazan <prielhaz@gmail.com>
2020-11-23 02:34:09 +0200

    added email functionality

---
+++ src/components/contactForm.js
@@ -0,0 +1,41 @@
+import emailjs from "emailjs-com";
+import React from 'react';
+
+export default function ContactUs() {
+    function sendEmail(e) {
+      console.log("sends mail");
+        e.preventDefault();
+    emailjs.sendForm('service_gk688ip', 'template_t8bvc8d', e.target, 'user_LLQ9jJYKS5bbiRNCrgoJ7')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    }
+    return(
+        <div>
+            <div className="container">
+            <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name" id="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email" id="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Subject" name="phone" id="phone"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message" id="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
