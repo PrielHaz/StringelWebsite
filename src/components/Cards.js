a394b78e3a3b31845451da98afe9a93d7943d542
PrielHazan <prielhaz@gmail.com>
2020-11-25 16:44:59 +0200

    Stringel website

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,34 +10,34 @@ function Cards() {
 				<div className="cards__wrapper">
 					<ul className="cards__items">
 						<CardItem
-							src="images/img-9.jpg"
-							text="Explore the hidden waterfall deep inside the Amazon Jungle"
-							label="Adventure"
+							src="images/Assasination.png"
+							text="Game Development"
+							label="Gaming"
 							path="/services"
 						/>
 						<CardItem
-							src="images/img-2.jpg"
-							text="Travel through the Islands of Bali in a Private Cruise"
-							label="Luxury"
+							src="images/Web.jpg"
+							text="Web Development"
+							label="Web"
 							path="/services"
 						/>
 					</ul>
 					<ul className="cards__items">
 						<CardItem
-							src="images/img-3.jpg"
-							text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-							label="Mystery"
+							src="images/SmartBuild.png"
+							text="Mobile Apps"
+							label="Mobile"
 							path="/services"
 						/>
 						<CardItem
-							src="images/img-4.jpg"
-							text="Experience Football on Top of the Himilayan Mountains"
-							label="Adventure"
+							src="images/git.png"
+							text="Projects"
+							label="Git"
 							path="/products"
 						/>
 						<CardItem
-							src="images/img-8.jpg"
-							text="Ride through the Sahara Desert on a guided camel tour"
+							src="images/resume.jpg"
+							text="Our programmers, skills, resume, specialties and more"
 							label="Adrenaline"
 							path="/sign-up"
 						/>
